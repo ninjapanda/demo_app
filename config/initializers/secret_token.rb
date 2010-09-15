111a1ee8504b8e3c977d847ca1f2a4a0c791fbe6
Gabriel Chan <gabriel@thesportschimp.com>
2010-09-15 19:41:42 -0400

    initial committ

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'ffc97d2f3523ea282320d938e9cf1c3fa87eeaf73ab492e86cdbb31494dd2c5a0a52753d029b684f43633be65821156888c690c237871f6845ff5ce4feff63ba'
